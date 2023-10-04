2506d4a7e9c42959b46c79109ef1af4bedc27665
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2023-10-04 00:50:52 -0300

    feat(page): Create home page

---
+++ src/app/components/CardPokemonHome.js
@@ -0,0 +1,20 @@
+'use client'
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+
+export default function CardPokemonHome({ pokemon }) {
+    return (
+        <>
+            <Card style={{ width: '15rem' }}>
+                <Card.Body className='text-center text-capitalize'>
+                    <Card.Title>{pokemon.name}</Card.Title>
+                    <Button variant="dark">
+                        <Link className='text-decoration-none text-white' href={`${pokemon.name}`}>Detalhes</Link>
+                    </Button>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,19 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import CardPokemonHome from "./components/CardPokemonHome";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0");
+  const data = await response.json();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="container mt-5 mb-5">
+      <h1 className="text-center mb-4">Todos os pokémons</h1>
+      <div className="row">
+        {data.results.map((pokemon, index) => (
+          <div className="col-lg-3 col-md-6 col-12 mb-3 d-flex justify-content-center">
+            <CardPokemonHome pokemon={pokemon} index={index} />
+          </div>
+        ))}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
